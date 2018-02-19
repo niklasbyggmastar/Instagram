1691ad0eca8aff342acb5774b124f99bfbe07400
Niklas Byggmästar <niklas.byggmastar@gmail.com>
2018-02-19 14:47:02 +0200

    'fixd'

--- app/views.py
+++ app/views.py
@@ -86,7 +86,7 @@ def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid:
-            form.save()
+            form.save
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=raw_password)
