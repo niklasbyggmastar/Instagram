78d2dc35ddd8058665ebe8ff0a4f7ec7a950279c
Niklas Byggmästar <niklas.byggmastar@gmail.com>
2018-02-19 15:30:12 +0200

    'fixd'

--- instagram/settings.py
+++ instagram/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nmb-instagram',
-        'USER': 'postgres',
-        'PASSWORD': 'Julla080997!',
-        'HOST': 'localhost',
+        'USER': 'amggpflxxrxejk',
+        'PASSWORD': '7e929006f0c79eb7f4bb2b856ca7b44134204026cf5722821fb65abdc9e31ad8',
+        'HOST': 'ec2-174-129-221-240.compute-1.amazonaws.com',
         'PORT': '5432'
     }
 }
