a3aa3b284d6964e35aa43c1a4908803c1d8ed360
Niklas Byggmästar <niklas.byggmastar@gmail.com>
2018-02-21 14:31:28 +0200

    settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
